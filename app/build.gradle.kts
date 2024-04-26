64e7c4e158516436b26db0e32d53fd7af5e0680f
David Bauer <mail@david-bauer.net>
2024-04-26 20:08:20 +0200

    meta: bump version to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "net.freifunk.darmstadt.nodewhisperer"
         minSdk = 30
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
