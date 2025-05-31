d27a35c74538a4ad28422cf223c06b22044abf93
David Bauer <mail@david-bauer.net>
2025-05-31 17:26:08 +0200

    meta: release v1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "net.freifunk.darmstadt.nodewhisperer"
         minSdk = 30
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
